4e4d6d53a161ec7589a7b293edab11308b8c9fb5
Diogo Pontes <dpontes@protonmail.com>
2023-12-13 14:40:22 +0100

    Added l as Git alias for log

--- setup.sh
+++ setup.sh
@@ -24,6 +24,7 @@ git config --global alias.st status
 git config --global alias.pl pull
 git config --global alias.d diff
 git config --global alias.aA "add ."
+git config --global.alias.l log
 
 # Add all the extra bash commands to ~/.bashrc
 cat extra-bash >> ~/.bashrc
