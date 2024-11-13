89c0072c80f66440611491a4622b843108652b48
DPontes <dpontes@protonmail.com>
2024-11-13 08:53:25 +0100

    simplification of init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
-vim.g.mapleader = " "
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -13,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -22,74 +15,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Setup vim commands
+require("vim-commands")
 
 -- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-      "nvim-telescope/telescope.nvim", tag = "0.1.8",
-      dependencies = { "nvim-lua/plenary.nvim" }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-      }
-    },
-    {
-      'nvim-lualine/lualine.nvim',
-      dependencies = { 'nvim-tree/nvim-web-devicons' },
-      options = { theme = dracula},
-    },
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-    {'neovim/nvim-lspconfig'},
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "captppuccin-mocha" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
-
-require('lualine').setup()
-require('mason').setup()
-require('mason-lspconfig').setup({
-  ensure_installed = { 'lua_ls', 'clangd' }
-  })
-
-local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup({})
-lspconfig.clangd.setup({})
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', { desc = 'Telescope reveal file in Neotree' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-vim.keymap.set('n', '<leader>c', ':Neotree<CR>', {})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin-mocha"
+require("lazy").setup("plugins")
