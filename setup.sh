ca0aafb2226c80186929abd21c6cc97b074e1d02
DPontes <dpontes@protonmail.com>
2025-01-21 10:20:21 +0100

    Add installation for Lazygit

--- setup.sh
+++ setup.sh
@@ -8,6 +8,14 @@ then
     chmod +x ~/.tmux.conf
 fi
 
+# Add .bash_aliases
+if [[ ! -f ~/.bash_aliases ]]
+then
+	echo "Adding bash_aliases" && \
+	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases && \
+	source ~/.bash_aliases
+fi
+
 # git alias configuration
 echo "Adding additional git config"
 
@@ -18,14 +26,6 @@ git config --global alias.aA "add ."
 git config --global alias.l log
 git config --global alias.b branch
 git config --global core.editor	"nvim"
-
-# Add .bash_aliases
-if [[ ! -f ~/.bash_aliases ]]
-then
-	echo "Adding bash_aliases" && \
-	ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases && \
-	source ~/.bash_aliases
-fi
 
 # Install fzf (Fuzzy Finder)
 if [[ ! -d ~/.fzf/ ]]
@@ -45,6 +45,16 @@ then
   # the following commands are necessary because "bat" is installed as "batcat" due to name clashing
   mkdir -p ~/.local/bin && \
   ln -s /usr/bin/batcat ~/.local/bin/bat
+fi
+
+# Install lazygit
+if [[ ! -f /usr/local/bin/lazygit ]]
+then
+  echo "Installing Lazygit..." && \
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*') && \
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" && \
+  tar xf lazygit.tar.gz lazygit && \
+  rm lazygit.tar.gz
 fi
 
 # Move neovim config to ~/.config
