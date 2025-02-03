1f5e73bcdbb1c103fe9173a5fda5bbe5e404bb39
DPontes <dpontes@protonmail.com>
2025-02-03 13:01:22 +0100

    Updated render-markdown
    Attempted to add custom mini-icons for the TODO list, but not yet
    successful

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,9 +1,20 @@
 return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', },
+    dependencies = { 'nvim-treesitter/nvim-treesitter',{ 'echasnovski/mini.icons', version = '*'} },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
-  },
+    config = function()
+      require('mini.icons').setup()
+      require('render-markdown').setup({
+        checkbox = {
+          unchecked = { icon = '✘ ' },
+          checked = { icon = '✔ ', scope_highlight = '@markup.strikethrough' },
+          custom = { todo = { rendered = '◯ ' }
+          },
+        }
+      })
+    end
+  }
 }
