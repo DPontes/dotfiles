9eca607152849099dc7521923f6bf57ef621a1b3
DPontes <dpontes@protonmail.com>
2025-02-03 10:29:19 +0100

    Small fixes to datetime plugin

--- nvim/lua/plugins/datetime.lua
+++ nvim/lua/plugins/datetime.lua
@@ -4,8 +4,17 @@ return {
     version = '*',
     lazy = false,
     config = function()
-      vim.keymap.set("n", "<leader>rd", ':r! date "+\\%d-\\%m-\\%Y" <CR>', {})
-      vim.keymap.set("n", "<leader>rt", ':r! date "+\\%H:\\%M:\\%S" <CR>', {})
+      local date_time_inserter = require("date-time-inserter")
+      date_time_inserter.setup({
+        date_format = 'DDMMYYYY',
+        date_separator = '/',
+        date_time_separator = ' @ ',
+        time_format = 24,
+        show_seconds = false
+      })
+      vim.keymap.set("n", "<leader>dt", ":InsertDate<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>tt", ":InsertTime<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "<leader>dtt", ":InsertDateTime<CR>", { noremap = true, silent = true })
     end
   },
 }
