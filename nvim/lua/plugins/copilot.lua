f0e60597a9f4645128f03d0e39cf5269748bcb96
DPontes <dpontes@protonmail.com>
2026-02-06 11:49:47 +0100

    Fixed Copilot panel

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,10 +4,37 @@ return {
     event = "InsertEnter",
     cmd = "Copilot",
     config = function()
-      require("copilot").setup({
-        suggestion = { enabled = true },
-        panel = { enabled = true },
-      })
+    require('copilot').setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true, -- Let it search automatically when you open it
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<leader>cp" -- Use the leader key we discussed
+        },
+        layout = {
+          position = "right",
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      -- Use default filetypes (most code files are enabled by default)
+      filetypes = {
+        ["*"] = true, -- This enables it for EVERYTHING; use with caution
+      },
+    })
     end,
     dependencies = {
       "zbirenbaum/copilot-cmp",
