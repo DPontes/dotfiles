d08ab5826a297816585e5a2e72da390367a64e21
DPontes <dpontes@protonmail.com>
2026-02-23 10:13:59 +0100

    Additional fixes to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
+    # Auto-start tmux
+    if type -q tmux; and not set -q TMUX
+        exec tmux
+    end
+
     # Vi mode
     fish_vi_key_bindings
+
+    # fzf key bindings (Ctrl+R for history search)
+    fzf_key_bindings
 
     # Remap Caps Lock to Escape
     if type -q setxkbmap
